181ff374a201cb44ef6e80b6652174ef92a79fc7
Parth Patadia <parthpatadia29@gmail.com>
2024-07-05 15:08:13 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
-const express = require('express')
-const app = express()
-const db = require('./db');
-require('dotenv').config();
-const passport = require('./auth')
+const express = require("express");
+const app = express();
 
-const bodyParser = require('body-parser');
+const db = require("./db");
+require("dotenv").config();
+const passport = require("./auth");
+
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
-const logRequest = (req,res,next)=>{
-  console.log('[$({new Date().toLocaleString()}] Request made to : ${req.originalURL}');
+const logRequest = (req, res, next) => {
+  console.log(
+    `[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`
+  );
   next();
-}
+};
 app.use(logRequest);
 
 app.use(passport.initialize());
-const localAuthMiddleware = passport.authenticate('local',{session: false});
+const localAuthMiddleware = passport.authenticate("local", { session: false });
 
 
-const MenuItem = require('./models/MenuItem');
+app.get("/", function (req, res) {
+  res.send("Welcome to our Hotel!");
+});
 
+const personRoutes = require("./routes/personRoutes");
+const menuItemRoutes = require("./routes/menuItemRoutes");
 
-app.get('/', function (req, res) {
-  res.send('Welcome to our Hotel!');
-})
+app.use("./person", personRoutes);
+app.use("/menu", menuItemRoutes);
 
-const personRoutes = require('./routes/personRoutes');
-const menuItemRoutes = require('./routes/menuItemRoutes');
-
-app.use('./person',personRoutes);
-app.use('/menu',menuItemRoutes);
-
-
-
-app.listen(PORT, ()=>{
-  console.log('Listening on port 3000');
-})
+app.listen(PORT, () => {
+  console.log("Listening on port 3000");
+});
